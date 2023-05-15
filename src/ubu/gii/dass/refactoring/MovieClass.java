5224fb661a0b14a3786b5f2193bab9f3a5609d62
jrp1004 <jrp1004@alu.ubu.es>
2023-05-15 18:50:27 +0200

    Extraído clase película #2
    Se han extraído las constantes que diferenciaban cada clase de película
    al enum MovieClass. Cada posible valor de la enumeración tiene asociado
    el precio, el número de días a partir de los que se aplica el
    multiplicador y el multiplicador correspondientes

---
+++ src/ubu/gii/dass/refactoring/MovieClass.java
@@ -0,0 +1,28 @@
+package ubu.gii.dass.refactoring;
+
+public enum MovieClass {
+	CHILDREN(1.5,3,1.5), REGULAR(2,2,1.5), NEW_RELEASE(0,0,3);
+	
+	private double price;
+	private int dias;
+	private double multiplicador;
+	
+	private MovieClass(double price, int dias, double multiplicador) {
+		this.price=price;
+		this.dias=dias;
+		this.multiplicador=multiplicador;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getDias() {
+		return dias;
+	}
+
+	public double getMultiplicador() {
+		return multiplicador;
+	}
+	
+}
